cac70c15e25181f41c0bd600ff4fc049e2cdabd1
Noteworthy Software, Inc <online@noteworthysoftware.com>
2016-01-25 15:20:25 -0500

    Change nwsw_ArpeggiateChord to use the Arpeggio.ms object

--- nwsw_ArpeggiateChord.php
+++ nwsw_ArpeggiateChord.php
@@ -1,17 +1,18 @@
 <?php
 /*******************************************************************************
-nwsw_ArpeggiateChord.php Version 1.21
+nwsw_ArpeggiateChord.php Version 2
 
-This script will arpeggiate selected chords by adding grace notes that tie into the chord.
+This script will arpeggiate selected chords using the Arpeggio.ms object.
 
 Prompting can be added with the following command line:
-"/duration=<PROMPT:Select a grace note duration:=|8th|16th|32nd>" "/sequence=<PROMPT:Select an arpeggio order:=|up|down|random>"
+"/duration=<PROMPT:Select a duration:=|8th|16th|32nd>" "/sequence=<PROMPT:Select an arpeggio order:=|up|down>"
 
-Copyright © 2007 by NoteWorthy Software, Inc.
+Copyright © 2016 by NoteWorthy Software, Inc.
 All Rights Reserved
 
 HISTORY:
 ================================================================================
+[2016-01-25] Version 2:    Switch from grace notes to use of Arpeggio.ms object
 [2007-06-19] Version 1.21: Support prompting
 [2007-06-16] Version 1.20: Add grace note duration constant, sequencing scheme, and ignore last note in chord sequence
 [2006-05-12] Version 1.01: Code format cleanup
@@ -21,8 +22,9 @@ require_once("lib/nwc2clips.inc");
 
 // ARPEGGIO_DURATION can be one of 8th, 16th, 32nd, 64th
 $ARPEGGIO_DURATION = "8th";
+$ARPDURConvert = array('8th'=>8,'16th'=>16,'32nd'=>32,'64th'=>64);
 //
-// Const_ARPEGGIO_SEQUENCE can be one of up, down, or random
+// Const_ARPEGGIO_SEQUENCE can be one of up or down
 $ARPEGGIO_SEQUENCE = "up";
 
 foreach ($argv as $k => $v) {
@@ -36,55 +38,49 @@ if (count($clip->Items) < 1) trigger_error("Please select at least one chord",E_
 
 echo $clip->GetClipHeader()."\n";
 
-$priorNoteObj = false;
+$ArpeggioAllowedHere = true;
 foreach ($clip->Items as $item) {
 	$o = new NWC2ClipItem($item);
 	//
 	// If this is a non-grace chord and is not preceded by a grace note,
-	// and it is larger than an 8th note in duration, then add a grace note arpeggio
+	// and it is larger than an 8th note in duration, then add an arpeggio
 	if (($o->GetObjType() == "Chord") && 
-			!isset($o->Opts["Dur"]["Grace"]) &&
-			(count(array_intersect(array_keys($o->Opts["Dur"]),array("Whole","Half","4th"))) > 0) &&
-			!isset($priorNoteObj->Opts["Dur"]["Grace"])) {
+		!isset($o->Opts["Dur"]["Grace"]) &&
+		(count(array_intersect(array_keys($o->Opts["Dur"]),array("Whole","Half","4th"))) > 0) &&
+		$ArpeggioAllowedHere)
+		{
+		// Create the Arpeggio.ms object, then update its properties as needed
+
+		$userObj = new NWC2ClipItem('|User|Arpeggio.ms|Pos:-4|Class:Standard|Dir:up|Rate:16|Color:0|Visibility:Default');
+
 		$chordnotes = $o->GetTaggedOpt("Pos");
-		$chorddur = $o->GetTaggedOpt("Dur");
-
-		switch ($ARPEGGIO_SEQUENCE) {
-		case "up":
-			// do nothing
-			break; 
-
-		case "down":
-			$chordnotes = array_reverse($chordnotes);
-			break;
-
-		case "random":
-			shuffle($chordnotes);
-			break;
-		}
-
-		// The last item does not need a tied in grace note
-		array_pop($chordnotes);
-
-		foreach ($chordnotes as $i => $notepitchtxt) {
-			$notepitchObj = new NWC2NotePitchPos($notepitchtxt);
-			$notepitchObj->Tied = '^';
-
-			$new_notepitchtxt = $notepitchObj->ReconstructClipText();
-			//
-			$beamOpt = ($i ? "" : "=First");
-			if ($i == (count($chordnotes)-1)) $beamOpt = "=End";
-			//
-			$o = new NWC2ClipItem($item);
-			echo "|Note|Dur:$ARPEGGIO_DURATION,Grace|Opts:Stem=Up,Beam$beamOpt|Pos:$new_notepitchtxt\n";
+		$userObj->Opts['Pos'] = $chordnotes[0]['Position'];
+		
+		foreach (array('Color','Visibility') as $baseprop) {
+			if (!empty($o->Opts[$baseprop])) $userObj->Opts[$baseprop] = $o->Opts[$baseprop];
 			}
+		
+		if ($ARPEGGIO_SEQUENCE == "down") $userObj->Opts['Dir'] = 'down';
+		
+		$userObj->Opts['Rate'] = nw_aafield($ARPDURConvert,$ARPEGGIO_DURATION,16);
+
+		echo $userObj->ReconstructClipText()."\n";
+
+		// Now mute the chord
+		if (empty($o->Opts['Opts'])) $o->Opts['Opts'] = array();
+		$o->Opts['Opts']['Muted'] = '';
+		echo $o->ReconstructClipText()."\n";
+		}
+	else {
+		echo $item;
 		}
 
-	echo $item;
-
-	if (in_array($o->GetObjType(),array('Note','Chord','Rest','RestChord'))) $priorNoteObj = $o;
-	else if ($o->GetObjType() == "Bar") unset($priorNoteObj);
-	unset($o);
+	if ($o->GetUserObjType() == 'Arpeggio.ms')
+		$ArpeggioAllowedHere = false;
+	else if (in_array($o->GetObjType(),array('Note','Chord','Rest','RestChord')))
+		$ArpeggioAllowedHere = !isset($o->Opts["Dur"]["Grace"]);
+	else if ($o->GetObjType() == "Bar")
+		$ArpeggioAllowedHere = true;
 	}
 
 echo NWC2_ENDCLIP."\n";
